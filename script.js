ed340306c30f5b037e7806bbad6d4d28b1ed6128
DesingworkLA <desigworkerla@gmail.com>
2026-04-21 18:20:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,14 @@ document.querySelectorAll(".info-btn").forEach(btn => {
 });
 
 document.querySelectorAll(".ball-item").forEach((ball) => {
-  const colors = ["#ff3b3b","#2f7bff","#ffd400","#00d084","#ff7b00","#b400ff"];
+  const colors = [
+    "#ff3b3b",
+    "#2f7bff",
+    "#ffd400",
+    "#00d084",
+    "#ff7b00",
+    "#b400ff"
+  ];
 
   setInterval(() => {
     const random = colors[Math.floor(Math.random() * colors.length)];
